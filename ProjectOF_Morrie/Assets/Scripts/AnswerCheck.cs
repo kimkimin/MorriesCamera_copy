9bfb289c952da8aa795b2a5b1c9d1e4131d71b3b
kimminkyoung <somuon9981@daum.net>
2019-11-05 17:48:32 +0900

    edit AnswerGetList.cs
    정답 리스트 가져오기 및 해당 리스트 위치 및 코너정보 저장,전달
    AnswerCheck.cs / 위의 정보를 통해 드래그 영역내에 정답이 있는지 확인

---
+++ ProjectOF_Morrie/Assets/Scripts/AnswerCheck.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerCheck : MonoBehaviour
+{
+    public RectTransform boxImage;
+    public Vector2 boxSizeMin, boxSizeMax;
+
+    bool b_isSizeFit = false;
+
+    public void SizeCheckWhileDraging()
+    {
+        if(boxImage.sizeDelta.x > boxSizeMin.x && boxImage.sizeDelta.y >= boxSizeMin.y)
+        {
+            if(boxImage.sizeDelta.x < boxSizeMax.x && boxImage.sizeDelta.y < boxSizeMax.y)
+            {
+                b_isSizeFit = true;
+                boxImage.gameObject.GetComponent<Image>().color = Color.green * new Color(1, 1, 1, 0.5f);
+            }
+            else
+            {
+                b_isSizeFit = false;
+                boxImage.gameObject.GetComponent<Image>().color = Color.grey * new Color(1, 1, 1, 0.5f); ;
+            }
+        }
+    }
+
+    public void FigureOutDragEnded()
+    {   if (!enabled) return;
+        if (!b_isSizeFit) return;
+
+        //if
+    }
+
+    /*
+    /// <summary>
+    /// 드래그와 찾은이미지 비교
+    /// 손이 떨어졌을때 드래그박스가 이미지범위에서 벗어났는지를 확인
+    /// 1. 정가운데 위치 포인트가 포함되는지 확인
+    /// 2. true일경우 오답을 확인해서 
+    /// </summary>
+    public bool DragHideCompare(RectTransform CheckRecttransform)//
+    {
+        //정답 이미지의 모든 코너가 박스안에 들어왔을때로 수정하기
+        //0. 위치포인트 들어오는지 확인
+        //1. 정답 코너 리스트비교 > 위치비교, 각 코너비교
+        //2. 오답 코너 리스트비교 > 위치비교, 각 코너비교
+        //3. true false 체크하기
+
+        for (int i = 0; i < ins_tttr.RightObject.Count; i++)
+        {
+            if (RectTransformUtility.RectangleContainsScreenPoint(CheckRecttransform, ins_tttr.PositionFigureOut_Right()[i]))//position 확인
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (!RectTransformUtility.RectangleContainsScreenPoint(CheckRecttransform, ins_tttr.GetSizedeltaFromRender(ins_tttr.RightObject[i])[j]))//corner 확인
+                        return false;
+                }
+                return true;
+            }
+        }
+        return false;
+    }*/
+}
---
+++ ProjectOF_Morrie/Assets/Scripts/AnswerGetList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 정답 목록을 가져오고, 해당 목록의 위치값과 코너값 리스트를 return
+/// </summary>
+public class AnswerGetList : MonoBehaviour
+{
+    public GameObject[] rightAnswer;
+    Vector3[] rightPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightPos = new Vector3[rightAnswer.Length];
+    }
+
+    /// <summary>
+    /// rightAnswer의 위치정보 저장후 전달
+    /// </summary>
+    public Vector3[] GetAnswerPosition()
+    {
+        for (int i = 0; i < rightAnswer.Length; i++)
+        {
+            rightPos[i] = rightAnswer[i].GetComponent<Renderer>().bounds.center;
+        }
+        return rightPos;
+    }
+
+    /// <summary>
+    /// 중점에서 (사이즈/4)만큼 +-해서 실제이미지보다 안쪽으로 위치한 코너값을 리턴
+    /// return vector3[](각 코너의 리스트)
+    /// </summary>
+    /// <param name="spineObject"> 정답 혹은 오답 </param>
+    public Vector3[] GetSizedeltaFromRender(GameObject spineObject)
+    {
+        Bounds spineBounds = spineObject.GetComponent<Renderer>().bounds;
+        Vector3 spineSize = spineBounds.size / 4;//실제 이미지보다 더 작게하는 부분
+
+        Vector3 minFromPoint = new Vector3(spineBounds.min.x + spineSize.x, spineBounds.min.y + spineSize.y, spineObject.GetComponent<Transform>().position.z);
+        Vector3 maxFromPoint = new Vector3(spineBounds.max.x - spineSize.x, spineBounds.max.y - spineSize.y, spineObject.GetComponent<Transform>().position.z);
+
+        Vector3 corner1 = new Vector3(minFromPoint.x, maxFromPoint.y, spineObject.GetComponent<Transform>().position.z);
+        Vector3 corner4 = new Vector3(maxFromPoint.x, minFromPoint.y, spineObject.GetComponent<Transform>().position.z);
+
+        Vector3[] spineCorner = { corner1, maxFromPoint, minFromPoint, corner4 };
+
+        return spineCorner;
+    }
+}
