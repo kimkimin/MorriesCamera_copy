7f19036f01e1f1403e6572093df53ba9cd844880
kimminkyoung <somuon9981@daum.net>
2019-12-12 10:15:58 +0900

    add spine

---
+++ ProjectOF_Morrie/Assets/Scripts/Spine_TouchSkin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class Spine_TouchSkin : MonoBehaviour
+{
+    //public string[] skins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<SkeletonAnimation>().skeleton.
+        //print();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
