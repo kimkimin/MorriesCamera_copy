680e476da97201b15d5ebdf70e33fa6b58f0d214
kimminkyoung <somuon9981@daum.net>
2019-12-16 10:51:07 +0900

    add spine, startSceneAsset
    > 스타트씬 추가
    > ios플러그인 (햅틱진동) 수정
    > 스파인 추가

--- ProjectOF_Morrie/Assets/Scripts/Haptic/binding.cs
+++ ProjectOF_Morrie/Assets/Scripts/Haptic/binding.cs
@@ -4,7 +4,40 @@ using UnityEngine;
 
 public class binding : MonoBehaviour
 {
-    public static void LigthHaptic(){
+    public static bool HapticActive = false;
+
+
+    public static void LigthHaptic()
+    {
         iOSPlugin._unityHaptic("Light");
     }
+    public static void MediumHaptic()
+    {
+        iOSPlugin._unityHaptic("Medium");
+    }
+    public static void HeavyHaptic()
+    {
+        iOSPlugin._unityHaptic("Heavy");
+    }
+    public static void SuccessHaptic()
+    {
+        iOSPlugin._unityHaptic("Success");
+    }
+    public static void WarningHaptic()
+    {
+        iOSPlugin._unityHaptic("Warning");
+    }
+    public static void ErrorHaptic()
+    {
+        iOSPlugin._unityHaptic("Error");
+    }
+
+    public void HapticControl()
+    {
+        if (HapticActive)
+            HapticActive = false;
+        else
+            HapticActive = true;
+    }
 }
+//지금 너가 보는시점으로 저번주에 이거함 실행안시켜봄 해보셈 넹~
