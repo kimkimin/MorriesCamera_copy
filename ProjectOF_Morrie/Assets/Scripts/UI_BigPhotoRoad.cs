3b09054edfacbd05d11dcdaebaf06a3ec52b1c30
kimminkyoung <somuon9981@daum.net>
2019-12-23 16:15:10 +0900

    저장된 사진확대
    UI_BigPhotoRoad.cs / 저장된 사진을 큰화면에 표시

---
+++ ProjectOF_Morrie/Assets/Scripts/UI_BigPhotoRoad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_BigPhotoRoad : MonoBehaviour
+{
+    //Texture2D getTexture;
+    public Image setImage;
+    
+    public void SetImageTexture()
+    {
+        setImage.sprite = gameObject.GetComponent<Image>().sprite;
+    }
+}
