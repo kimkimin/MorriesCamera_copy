ae407cd565d716271dd71888324b799f5fffc06c
kimminkyoung <somuon9981@daum.net>
2019-12-24 15:34:29 +0900

    EndingScene 카툰
    페이지 슬라이더

---
+++ ProjectOF_Morrie/Assets/Scripts/UI_EndingPageSlider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_EndingPageSlider : MonoBehaviour
+{
+    public GameObject[] pages;
+    int pagesNum = 1;
+    bool pageEnd = true;
+    
+
+    public void NextPage()
+    {
+        print("??");
+        if (pagesNum >= pages.Length) return;
+        if (!pageEnd) return;
+
+        pages[pagesNum].SetActive(true);
+        pagesNum++;
+        pageEnd = false;
+    }
+
+    public void CheckPageStart()
+    {
+        pageEnd = true;
+    }
+}
