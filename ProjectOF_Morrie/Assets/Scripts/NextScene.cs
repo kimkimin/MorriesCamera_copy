fd921f7485d38e4ffc6b298216b493ea28e214f9
kimminkyoung <somuon9981@daum.net>
2019-12-26 16:24:35 +0900

    브러쉬 테스트도 추가

---
+++ ProjectOF_Morrie/Assets/Scripts/NextScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public int SceneNum;
+
+    public void GoSceneWithNum()
+    {
+        SceneManager.LoadScene(SceneNum);
+    }
+}
