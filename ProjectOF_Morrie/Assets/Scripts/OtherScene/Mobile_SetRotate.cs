7d29b676c7fef3a3b2b1c22e058b218764d9f923
kimminkyoung <somuon9981@daum.net>
2020-01-06 17:57:44 +0900

    스타트 씬 ~ 스테이지1 엔딩까지 연결
    1. 스타트씬 진행중

---
+++ ProjectOF_Morrie/Assets/Scripts/OtherScene/Mobile_SetRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mobile_SetRotate : MonoBehaviour
+{
+    public bool isPortrait = false;
+    public bool isLandscape = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isPortrait)
+            Screen.orientation = ScreenOrientation.Portrait;
+        else if (isLandscape)
+            Screen.orientation = ScreenOrientation.Landscape;
+    }
+    
+}
