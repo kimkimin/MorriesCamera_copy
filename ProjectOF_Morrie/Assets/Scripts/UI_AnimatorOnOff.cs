f8482a3c3dacb62e36ea0c47a7adf01874d04b1e
kimminkyoung <somuon9981@daum.net>
2020-01-16 17:45:08 +0900

    ui 수정
    > 인게임 ui 가이드 이미지 추가
    > 인게임 setting 애니메이션 수정
    > 인터랙션 수정중
    >

---
+++ ProjectOF_Morrie/Assets/Scripts/UI_AnimatorOnOff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_AnimatorOnOff : MonoBehaviour
+{
+    public Animator targetAnim;
+
+    public void SetTargetAnimOn()
+    {
+        targetAnim.enabled = true;
+    }
+    public void SetTargetAnimOff()
+    {
+        targetAnim.enabled = false;
+    }
+
+    public void SetMyAnimOn()
+    {
+        GetComponent<Animator>().enabled = true;
+    }
+    public void SetMyAnimOff()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+}
